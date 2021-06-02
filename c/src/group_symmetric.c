c5f345538e5e7f373f3070e25816be47ce540dc7
Tom Ross <tom.ross@protonmail.com>
2021-06-02 15:56:11 +0800

    [UPDATE] Permutation visualisation

--- c/src/group_symmetric.c
+++ c/src/group_symmetric.c
@@ -6,21 +6,12 @@
 
 #include "group_symmetric.h"
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
-#include <assert.h>
-
-// integer type
-typedef unsigned int u_int;
-
-
 // permutation types
 
 // transposition:
 // - transpose `left -> right`, `right -> left`
+// constatins:
+// - `left`, `right` must be distinct
 // notes:
 // - a transposition is simply a cycle with `length = 2`
 // - the ordering of `left`, `right` is arbitrary, so we choose `left < right`
@@ -31,12 +22,12 @@ struct Transposition
   u_int left;
   u_int right;
 };
-typedef struct Transposition Transposition;
 
 // cycle:
 // - cycle `length` unsigned integers, with `map[i] -> map[i+1]` for all
 //   `i in S(length-1)` and `map[length] -> map[0]`
 // constatins:
+// - `length` must be at least 2
 // - `map` must have `length` elements
 // example:
 // - `Cycle c = {3, {1, 5, 3}}` corresponds to the cycle `(1 5 3)` in cycle
@@ -46,7 +37,6 @@ struct Cycle
   u_int length;
   u_int * map;
 };
-typedef struct Cycle Cycle;
 
 // permutation:
 // - permute `degree` unsigned integers, with `i -> map[i]` for all
@@ -63,7 +53,7 @@ struct Permutation
   u_int degree;
   u_int * map;
 };
-typedef struct Permutation Permutation;
+
 
 // actions of permutations types
 
@@ -95,7 +85,7 @@ u_int cycle(Cycle const * const c, u_int const i)
     {
       return (c->map[ii+1]);
     }
-    ++ii;
+    ii++;
   }
 
   // case of `i` being the last element of the cycle
@@ -122,17 +112,115 @@ u_int permute(Permutation const * const p, u_int const i)
 // view
 
 // view_transposition:
-const char * view_transposition(Transposition const * const t)
+char * view_transposition(Transposition const * const t)
 {
+  assert(valid_transposition(t->left, t->right));
+
   // format string
-  const char fmt[] = "(%i %i)";
+  char const fmt[] = "(%i %i)";
 
   // safely determine size of `str` needed
-  const int len = snprintf(NULL, 0, fmt, t->left, t->right);
-  char * str = (char *)malloc(sizeof(char) * (size_t)(len + 1));
-  const int remaining = snprintf(str, 0, fmt, t->left, t->right);
+  int const len = snprintf(NULL, 0, fmt, t->left, t->right);
 
-  assert(remaining == 0);
+  // construct string
+  char * str = (char *)malloc(sizeof(char) * (size_t)(len + 1));
+  int const mark = snprintf(str, (size_t)(len + 1), fmt, t->left, t->right);
+
+  // ensure all characters were written
+  assert((size_t)mark == strlen(str));
+
+  return str;
+}
+
+// view_cycle:
+char * view_cycle(Cycle const * const c)
+{
+  assert(valid_cycle(c->length, c->map));
+
+  // format string
+  char const fmt_l[] = "(";
+  char const fmt_element[] = "%i";
+  char const fmt_between[] = " ";
+  char const fmt_r[] = ")";
+
+  // safely determine size of `str` needed
+  int len = snprintf(NULL, 0, fmt_l);
+  for (u_int ii = 0; ii < c->length; ii++)
+  {
+    len += snprintf(NULL, 0, fmt_element, c->map[ii]);
+
+    if (ii < c->length - 1)
+    {
+      len += snprintf(NULL, 0, fmt_between);
+    }
+  }
+  len += snprintf(NULL, 0, fmt_r);
+
+  // construct string
+  char * str = (char *)malloc(sizeof(char) * (size_t)(len + 1));
+
+  int mark = snprintf(str, (size_t)(len+1), fmt_l);
+  for (u_int ii = 0; ii < c->length; ii++)
+  {
+    mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_element, c->map[ii]);
+
+    if (ii < c->length - 1)
+    {
+      mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_between);
+    }
+
+  }
+  mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_r);
+
+  // ensure all characters were written
+  assert((size_t)mark == strlen(str));
+
+  return str;
+}
+
+// view_permutation:
+char * view_permutation(Permutation const * const p)
+{
+  assert(valid_permutation(p->degree, p->map));
+
+  // format string
+  char const fmt_l[] = "(";
+  char const fmt_element[] = "(%i->%i)";
+  char const fmt_between[] = " ";
+  char const fmt_r[] = ")";
+
+  // safely determine size of `str` needed
+  int len = snprintf(NULL, 0, fmt_l);
+  for (u_int ii = 0; ii < p->degree; ii++)
+  {
+    len += snprintf(NULL, 0, fmt_element, ii, p->map[ii]);
+
+    if (ii < p->degree - 1)
+    {
+      len += snprintf(NULL, 0, fmt_between);
+    }
+  }
+  len += snprintf(NULL, 0, fmt_r);
+
+  // construct string
+  char * str = (char *)malloc(sizeof(char) * (size_t)(len + 1));
+
+  int mark = snprintf(str, (size_t)(len+1), fmt_l);
+  for (u_int ii = 0; ii < p->degree; ii++)
+  {
+    mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_element,
+                     ii, p->map[ii]);
+
+    if (ii < p->degree - 1)
+    {
+      mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_between);
+    }
+
+  }
+  mark += snprintf(str+mark, (size_t)(len-mark+1), fmt_r);
+
+  // ensure all characters were written
+  assert((size_t)mark == strlen(str));
 
   return str;
 }
@@ -142,8 +230,12 @@ const char * view_transposition(Transposition const * const t)
 // new_transposition:
 // notes:
 // - the ordering of `left`, `right` is arbitrary, so we choose `left < right`
-void new_transposition(Transposition * const t, u_int const left, u_int const right)
+void new_transposition(Transposition * const t, u_int const left,
+                       u_int const right)
 {
+  // ensure `left`, `right` are distinct
+  assert(valid_transposition(left, right));
+
   // order `t->left = min(left, right)`, `t->right = max(left, right)`
   if (left <= right)
   {
@@ -160,25 +252,9 @@ void new_transposition(Transposition * const t, u_int const left, u_int const ri
 // new_cycle:
 void new_cycle(Cycle * const c, u_int const length, u_int const * const map)
 {
-  // ensure all elements of `map` are unique
-  bool valid = true;
-  u_int ii = 0;
-
-  while (valid && ii < length)
-  {
-    u_int jj = ii + 1;
-
-    while (valid && jj < length)
-    {
-      // ensure `map[ii]` is unique in `map`
-      valid = valid && (map[ii] != map[jj]);
-      ++jj;
-    }
-
-    ++ii;
-  }
-
-  assert(valid);
+  // ensure `map` has at least 2 elements, and that all elements of `map` are
+  // unique
+  assert(valid_cycle(length, map));
 
   // set `c->length` to `length`
   c->length = length;
@@ -190,36 +266,18 @@ void new_cycle(Cycle * const c, u_int const length, u_int const * const map)
   // copy`c->map` from `map` if `length` is non-zero
   if (length > 0)
   {
-    c->map = (u_int *)malloc(sizeof(u_int) * length);
-    memcpy(c->map, map, length);
+    size_t const n_bytes = sizeof(u_int) * (size_t)length;
+    c->map = (u_int *)malloc(n_bytes);
+    memcpy(c->map, map, n_bytes);
   }
 }
 
 // new_permutation:
-void new_permutation(Permutation * const p, u_int const degree, u_int const * const map)
+void new_permutation(Permutation * const p, u_int const degree,
+                     u_int const * const map)
 {
   // ensure all elements of `map` are unique, and are in `S(degree)`
-  bool valid = true;
-  u_int ii = 0;
-
-  while (valid && ii < degree)
-  {
-    u_int jj = ii + 1;
-
-    // ensure `map[ii] in S(degree)`
-    valid = valid && (map[ii] < degree);
-
-    while (valid && jj < degree)
-    {
-      // ensure `map[ii]` is unique in `map`
-      valid = valid && (map[ii] != map[jj]);
-      ++jj;
-    }
-
-    ++ii;
-  }
-
-  assert(valid);
+  assert(valid_permutation(degree, map));
 
   // set `p->degree` to `degree`
   p->degree = degree;
@@ -231,8 +289,9 @@ void new_permutation(Permutation * const p, u_int const degree, u_int const * co
   // copy `p->map` from `map` if `degree` is non-zero
   if (degree > 0)
   {
-    p->map = (u_int *)malloc(sizeof(u_int) * degree);
-    memcpy(p->map, map, degree);
+    size_t const n_bytes = sizeof(u_int) * (size_t)degree;
+    p->map = (u_int *)malloc(n_bytes);
+    memcpy(p->map, map, n_bytes);
   }
 }
 
@@ -267,4 +326,70 @@ void free_permutation(Permutation * const p)
 
   // set `p->degree`
   p->degree = 0;
+}
+
+// validators
+
+// valid_transposition:
+bool valid_transposition(u_int const left, u_int const right)
+{
+  // ensure `left`, `right` are distinct
+  bool valid = (left != right);
+
+  return valid;
+}
+
+// valid_cycle:
+bool valid_cycle(u_int const length, u_int const * const map)
+{
+  bool valid = true;
+
+  // ensure `map` has at least 2 elements
+  valid = valid && (length >= 2);
+
+  // ensure all elements of `map` are unique
+  u_int ii = 0;
+
+  while (valid && ii < length)
+  {
+    u_int jj = ii + 1;
+
+    while (valid && jj < length)
+    {
+      // ensure `map[ii]` is unique in `map`
+      valid = valid && (map[ii] != map[jj]);
+      jj++;
+    }
+
+    ii++;
+  }
+
+  return valid;
+}
+
+// valid_permutation:
+bool valid_permutation(u_int const degree, u_int const * const map)
+{
+  // ensure all elements of `map` are unique, and are in `S(degree)`
+  bool valid = true;
+  u_int ii = 0;
+
+  while (valid && ii < degree)
+  {
+    u_int jj = ii + 1;
+
+    // ensure `map[ii] in S(degree)`
+    valid = valid && (map[ii] < degree);
+
+    while (valid && jj < degree)
+    {
+      // ensure `map[ii]` is unique in `map`
+      valid = valid && (map[ii] != map[jj]);
+      jj++;
+    }
+
+    ii++;
+  }
+
+  return valid;
 }
--- c/src/group_symmetric.h
+++ c/src/group_symmetric.h
@@ -7,4 +7,48 @@
 #ifndef GROUP_SYMMETRIC_H
 #define GROUP_SYMMETRIC_H
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+// unsigned integer type
+typedef unsigned int u_int;
+
+// permutation types
+typedef struct Transposition Transposition;
+typedef struct Cycle Cycle;
+typedef struct Permutation Permutation;
+
+// function declarations
+
+// action
+u_int transpose(Transposition const * const t, u_int const i);
+u_int cycle(Cycle const * const c, u_int const i);
+u_int permute(Permutation const * const p, u_int const i);
+
+// view
+char * view_transposition(Transposition const * const t);
+char * view_cycle(Cycle const * const c);
+char * view_permutation(Permutation const * const p);
+
+// new
+void new_transposition(Transposition * const t, u_int const left,
+                       u_int const right);
+void new_cycle(Cycle * const c, u_int const length, u_int const * const map);
+void new_permutation(Permutation * const p, u_int const degree,
+                     u_int const * const map);
+
+// free
+void free_transposition(Transposition * const t);
+void free_cycle(Cycle * const c);
+void free_permutation(Permutation * const p);
+
+// validators
+bool valid_transposition(u_int const left, u_int const right);
+bool valid_cycle(u_int const length, u_int const * const map);
+bool valid_permutation(u_int const degree, u_int const * const map);
+
 #endif
